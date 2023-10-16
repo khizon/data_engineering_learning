cd04838289f11594f84ea55c7ab80d0c6727eba5
Kiel <kielhizon@gmail.com>
2023-10-16 14:42:16 +0800

    scheduled ETL

--- src/anime_info.py
+++ src/anime_info.py
@@ -1,14 +1,13 @@
-import json
 import logging
 import os
 from utils import *
 
 def main():
     try:
-        ingest_top_airing_anime()
-        if os.path.exists(os.path.join(os.getcwd(), 'data', 'top_airing.parquet')):
-            update_top_airing_anime()
-        ingest_anime_info(debug=True)
+        # ingest_top_airing_anime()
+        # if os.path.exists(os.path.join(os.getcwd(), 'data', 'top_airing.parquet')):
+        #     update_top_airing_anime()
+        ingest_anime_info()
         if os.path.exists(os.path.join(os.getcwd(), 'data', 'anime_info.parquet')):
             update_anime_info()
 
---
+++ src/top_anime.py
@@ -0,0 +1,19 @@
+import logging
+import os
+from utils import *
+
+def main():
+    try:
+        ingest_top_airing_anime()
+        if os.path.exists(os.path.join(os.getcwd(), 'data', 'top_airing.parquet')):
+            update_top_airing_anime()
+        # ingest_anime_info(debug=True)
+        # if os.path.exists(os.path.join(os.getcwd(), 'data', 'anime_info.parquet')):
+        #     update_anime_info()
+
+    except Exception as e:
+        # Log any exceptions
+        logging.error('Error occurred: %s', str(e))
+
+if __name__ == "__main__":
+    main()
