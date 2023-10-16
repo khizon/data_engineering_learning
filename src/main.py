e364f47b7d2b701677cc16f916304c9922e5ccad
Kiel <kielhizon@gmail.com>
2023-10-16 11:39:59 +0800

    Actions test

---
+++ src/main.py
@@ -0,0 +1,23 @@
+import json
+import logging
+
+# Configure the logging settings
+logging.basicConfig(filename='../data/test.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    try:
+        # Read the contents of secrets/keys.json
+        with open('../secrets/keys.json', 'r') as file:
+            keys_data = json.load(file)
+
+        # Log the contents to data/test.log
+        logging.info('Contents of keys.json: %s', json.dumps(keys_data, indent=2))
+
+        # Your additional code logic here, if needed
+
+    except Exception as e:
+        # Log any exceptions
+        logging.error('Error occurred: %s', str(e))
+
+if __name__ == "__main__":
+    main()
